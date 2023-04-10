6bbafabd24e46054842471c572ceae7a51a6a92b
Egor Gumenyuk <egorka228nampizda@gmail.com>
2023-04-10 16:47:24 +0300

    Clean Architecture

---
+++ app/src/main/java/com/example/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,17 @@
+package com.example.shoppinglist.domain
+
+import androidx.lifecycle.LiveData
+
+interface ShopListRepository {
+
+    fun addShopItem(shopItem: ShopItem)
+
+    fun getShopList(): LiveData<List<ShopItem>>
+
+    fun getShopItem(id: Int): ShopItem
+
+    fun editShopItem(shopItem: ShopItem)
+
+    fun deleteShopItem(shopItem: ShopItem)
+
+}
