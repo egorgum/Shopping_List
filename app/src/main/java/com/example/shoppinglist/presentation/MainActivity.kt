7e7a134bc9521dfa98766945bdb7daa3e99b89af
Egor Gumenyuk <egorka228nampizda@gmail.com>
2023-04-28 17:05:19 +0300

    Fragments

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -1,40 +1,57 @@
 package com.example.shoppinglist.presentation
 
-import android.content.ContentValues
-import android.content.ContentValues.TAG
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintSet.Layout
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglist.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedListener {
 
-class MainActivity : AppCompatActivity() {
 
+    private var shopItemContainer: FragmentContainerView? = null //Контейнер фрагмента
     private lateinit var viewModel: MainViewModel
     private lateinit var myAdapter: ShopListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        shopItemContainer = findViewById(R.id.shop_item_container)
         setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         val buttonAddItem = findViewById<FloatingActionButton>(R.id.btAdd)
+        //Запуск активтити или фрагмента
         buttonAddItem.setOnClickListener {
-            val intent = ShopItemActivity.newIntentAddItem(this)
-            startActivity(intent)
+            if (isOnePainMode()){
+                val intent = ShopItemActivity.newIntentAddItem(this)
+                startActivity(intent)
+            }
+            else{
+                launchFragment(ShopItemFragment.newInstanceAddItem())
+            }
         }
         viewModel.shopList.observe(this){
             myAdapter.submitList(it)
         }
     }
+
+    //Вертикальный или горизонтальный экран
+    private fun isOnePainMode():Boolean{
+        return shopItemContainer == null
+    }
+
+    private fun launchFragment(fragment: Fragment){
+        supportFragmentManager.popBackStack()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.shop_item_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
     private fun setupRecyclerView(){
 
         val rvShopList = findViewById<RecyclerView>(R.id.rv)
@@ -82,8 +99,14 @@ class MainActivity : AppCompatActivity() {
     //Переход на экран изменений
     private fun setupClickListener() {
         myAdapter.shopItemClick = {
-            val intent = ShopItemActivity.newIntentEditItem(this,it.id)
-            startActivity(intent)
+            if (isOnePainMode()){
+                val intent = ShopItemActivity.newIntentEditItem(this,it.id)
+                startActivity(intent)
+            }
+            else{
+                launchFragment(ShopItemFragment.newInstanceEditItem(it.id))
+            }
+
         }
     }
 
@@ -92,5 +115,11 @@ class MainActivity : AppCompatActivity() {
         myAdapter.shopItemLongClick = {
             viewModel.changeStateShopItem(it)
         }
+    }
+
+    //Реализация интерфейса фрагмента
+    override fun onEditingFinished() {
+        Toast.makeText(this@MainActivity,"Success", Toast.LENGTH_SHORT).show()
+        supportFragmentManager.popBackStack()
     }
 }
